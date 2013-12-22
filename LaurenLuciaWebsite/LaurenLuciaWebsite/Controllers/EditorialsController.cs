6a9f152bd688cd4b9d885c3c899f0d4897de5744
Josh Bleggi <joshbleggi@yahoo.com>
2013-12-22 15:25:03 -0500

    Added Editorial pages for aw2014, Lingerie, and Resort

---
+++ LaurenLuciaWebsite/LaurenLuciaWebsite/Controllers/EditorialsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaurenLuciaWebsite.Controllers
+{
+    public class EditorialsController : Controller
+    {
+        //
+        // GET: /Editorials/
+
+        public ActionResult aw2014()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Editorials/Create
+
+        public ActionResult Lingerie()
+        {
+            return View();
+        }
+
+        public ActionResult Resort()
+        {
+            return View();
+        }
+    }
+}
